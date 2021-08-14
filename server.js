933deb8381bf3344be424e5092feac46b97ae02b
Nittin sharma <nittinsharma37@gmail.com>
2021-08-14 23:43:25 +0530

    another changes applied

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dotenv = require('dotenv');
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+// dot env port 
+dotenv.config({path: "config.env"});
+const PORT = process.env.PORT || 8000;
+
+// log request 
+app.use(morgan("tiny"));
+
+
+// parse request to body parser
+app.use(bodyparser.urlencoded({extended: true}));
+
+// set view engine 
+app.set("view engine", "ejs");
+// app.set("views", path.resolve(__dirname, "views"));
+
+// load assets
+
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+
+
+app.get('/' , (req , res)=>{
+
+   res.render("index");
+   res.end();
+
+});
+
+app.listen(PORT, () => {
+    console.log("Server listening on port : " + PORT);
+});
+
+
