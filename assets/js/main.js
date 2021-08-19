1c2f0b86004808e4c06fcbabc565149c9779d3c3
Nittin sharma <nittinsharma37@gmail.com>
2021-08-19 14:57:21 +0530

    sdfg

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,41 +2,41 @@ $("#add_user").submit(function(event){
     alert("Data inserted successfully!");
 });
 
-$("#update_user").submit(function(event) {
-    event.preventDefault();
-    let unindexedArray = $(this).serializeArray();
-    let data = {};
+// $("#update_user").submit(function(event) {
+//     event.preventDefault();
+//     let unindexedArray = $(this).serializeArray();
+//     let data = {};
 
-    $.map(unindexedArray, (n,i) => {
-        data[n["name"]] = n["value"];
-    });
-    // console.log(data);
+//     $.map(unindexedArray, (n,i) => {
+//         data[n["name"]] = n["value"];
+//     });
+//     // console.log(data);
 
-    let request = {
-        "url": `http://localhost:2000/api/users/${data.id}`,
-        "method": "PUT",
-        "data": data
-    };
+//     let request = {
+//         "url": `http://localhost:2000/api/users/${data.id}`,
+//         "method": "PUT",
+//         "data": data
+//     };
 
-    $.ajax(request).done(function(response){
-        alert("Data updated successfull !");
-    });
-});
+//     $.ajax(request).done(function(response){
+//         alert("Data updated successfull !");
+//     });
+// });
 
 
-if(window.location.pathname == "/"){
-    $ondelete = $(".table tbody td a.delete");
-    $ondelete.click(function(){
-        let id = $(this).attr("data-id");
-        let request = {
-            "url": `http://localhost:2000/api/users/${id}`,
-            "method": "DELETE"
-        };
-        if(confirm("Do you really want to delete this record ?")){
-            $.ajax(request).done(function(response){
-                alert("User deleted Successfully !");
-                location.reload();
-            });
-        }
-    });
-}
+// if(window.location.pathname == "/"){
+//     $ondelete = $(".table tbody td a.delete");
+//     $ondelete.click(function(){
+//         let id = $(this).attr("data-id");
+//         let request = {
+//             "url": `http://localhost:2000/api/users/${id}`,
+//             "method": "DELETE"
+//         };
+//         if(confirm("Do you really want to delete this record ?")){
+//             $.ajax(request).done(function(response){
+//                 alert("User deleted Successfully !");
+//                 location.reload();
+//             });
+//         }
+//     });
+// }
